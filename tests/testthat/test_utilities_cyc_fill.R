37ec9caafadb96f94ceccd8e3b7d0ff07caa6adb
Ulaş Onat Alakent <36856525+ulasonat@users.noreply.github.com>
2019-04-17 02:49:13 +0200

    Changing the content of a test

--- tests/testthat/test_utilities_cyc_fill.R
+++ tests/testthat/test_utilities_cyc_fill.R
@@ -9,7 +9,9 @@ test_that("new_cycFill with numerical data", {
 })
 
 test_that("new_cycFill with polynomail data", {
-  c = new_cycFill((polynomial(c(3, 6))), 7)
-  expect_equal(d, c(3,6,3,6,3,6,3))
+  c = new_cycFill((polynomial(c(3, 6))), 8)
+  expect_equal(c, c(3,6,3,6,3,6,3,6))
   
+  d = new_cycFill((polynomial(c(5,11))), 13)
+  expect_equal(d, c(5,11,5,11,5,11,5,11,5,11,5,11,5))
 })
