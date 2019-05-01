35235e2bb7f4719fa94fa901ac507db6a36a2908
Ulaş Onat Alakent <36856525+ulasonat@users.noreply.github.com>
2019-05-01 20:45:09 +0200

    Renaming

--- tests/testthat/test_utilities_rowMin_and_colMin.R
+++ tests/testthat/test_utilities_rowMin_and_colMin.R
@@ -1,21 +1,21 @@
 context("utils rowmin colmin")
 
-test_that("new_colMin with matrix", {
+test_that("colMin with matrix", {
   mtr1 = matrix(c(5:34), 5, 6)
-  a = new_colMin(mtr1)
+  a = colMin(mtr1)
   expect_equal(a, seq(5, 30, 5))
   
   mtr2 = matrix(seq(4, 23, 2), 2, 5)
-  b = new_colMin(mtr2)
+  b = colMin(mtr2)
   expect_equal(b, seq(4, 20, 4))
 })
 
-test_that("new_rowMin with matrix", {
+test_that("rowMin with matrix", {
   mtr1 = matrix(c(5:34), 5, 6)
-  a = new_rowMin(mtr1)
+  a = rowMin(mtr1)
   expect_equal(a, c(5:9))
   
   mtr2 = matrix(seq(4, 23, 2), 2, 5)
-  b = new_rowMin(mtr2)
+  b = rowMin(mtr2)
   expect_equal(b, c(4, 6))
 })
