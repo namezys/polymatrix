7f7cf4f3ac52326132a156876ead8ef2551bfd43
Ulaş Onat Alakent <36856525+ulasonat@users.noreply.github.com>
2019-05-28 01:34:48 +0200

    I hope it's correct this time

--- tests/testthat/test_trace.R
+++ tests/testthat/test_trace.R
@@ -28,12 +28,12 @@ test_that("tr.matrix", {
 
 test_that("tr.polyMatrix", {
   a <- polyMgen.d(3, 3, rawData=ch2pn(c("-3 + z^2", "2 + 4*z", "-z^2", 
-                                          "1", "2", "3 + z", 
-                                          "2*z", "0", "2 - 3*z"), "z"), byrow=TRUE, symb="z")
+                                        "1", "2", "3 + z", 
+                                        "2*z", "0", "2 - 3*z"), "z"), byrow=TRUE, symb="z")
   expect_equal(tr.polyMatrix(a), polynomial(c(1, -3, 1)))
   
   b <- polyMgen.d(2, 2, rawData=ch2pn(c("3 + z^2", "2 + 2*z", "-z^2", 
-                                          "1", "-3", "5 + z"), "z"), byrow=TRUE, symb="z")
+                                        "1", "-3", "5 + z"), "z"), byrow=TRUE, symb="z")
   expect_equal(tr.polyMatrix(b), polynomial(c(4, 0, 1)))
   
   
