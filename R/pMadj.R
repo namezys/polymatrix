a97237dee0b1884f93d23ccea6e5f92e11376fb1
Ulaş Onat Alakent <36856525+ulasonat@users.noreply.github.com>
2019-06-08 18:50:13 +0200

    pMadj | Rewriting and tests (#25)

--- R/pMadj.R
+++ R/pMadj.R
@@ -1,16 +1,23 @@
-pMadj <- # the adjugate of the given polynomial matrix
-function(pm)
-  { if(dim(pm)[1]!=dim(pm)[2]) stop("'pm' must be a square matrix")
-    k<-dim(pm)[1]
-    pd<-polyMconvert(pm,"polyMdlist") # convert to a 'polyMdlist' class object
-    padj<-pd # skeleton of the result
-    for(i in 1:k) 
-      for(j in 1:k) 
-        { p<-pMdet(pMsub(pd,-i,-j))*(-1)^(i+j)
-          padj$dlist[[j]][[i]] <- p
-          padj$degree[j,i] <- degree(p) }
-    return(padj)
+# the adjugate of the given polynomial matrix
+pMadj <- function(pm)
+{ 
+  if (dim(pm)[1] != dim(pm)[2]) {
+    stop("'pm' must be a square matrix")
   }
+  
+  k <- dim(pm)[1]
+  pd <- polyMconvert(pm, "polyMdlist") # convert to a 'polyMdlist' class object
+  padj <- pd # skeleton of the result
+  for(i in 1:k) {
+    for(j in 1:k) {
+      p <- pMdet(pMsub(pd, -i, -j)) * (-1) ^ (i + j)
+      padj$dlist[[j]][[i]] <- p
+      padj$degree[j, i] <- degree(p) 
+    }
+  }
+  
+  return(padj)
+}
 
 
   
