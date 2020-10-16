ba374546733e8bd1c5afaf3581aafd200c28b49a
Nikolai Ryzhkov <namezys@gmail.com>
2020-10-17 00:25:39 +0200

    deggree

---
+++ R/method_degree.R
@@ -0,0 +1,21 @@
+# Title     : Get degree of polynomail matrix: max degree, degree matrix
+# Created by: namezys
+# Created on: 2020. 10. 17.
+
+#' Get maximum degree
+setGeneric("degree", function(x) {
+  stop(paste("Unknown object type:", class(x)[1]))
+})
+
+setMethod("degree", signature(x = NUM), function(x) {
+  if(length(x) != 1) {
+    stop("Numeric suquence is unsupported")
+  }
+  return(0)
+})
+
+setMethod("degree", signature(x = M), function(x) { matrix(0, nrow(x), ncol(x)) })
+
+setMethod("degree", signature(x = P), function(x) { length(x) - 1 })
+
+setMethod("degree", signature(x = PM), function(x) { as.integer(ncol(x@coef) / x@ncol - 1) })
---
+++ tests/testthat/test_method_degree.R
@@ -0,0 +1,29 @@
+context("method degree")
+
+p <- polynom::polynomial
+
+test_that("invalid request", {
+  expect_error(degree("foo"))
+  expect_error(degree(TRUE))
+  expect_error(degree(c(1, 2)))
+})
+
+test_that("degree of number", {
+  expect_equal(degree(1), 0)
+})
+
+test_that("degree of matrix", {
+  expect_equal(degree(matrix(1:6, 2, 3)), matrix(0, 2, 3))
+})
+
+test_that("degree of polynomial", {
+  expect_equal(degree(p(0)), 0)
+  expect_equal(degree(p(c(1, 2, 3))), 2)
+  expect_equal(degree(p(0:1000)), 1000)
+})
+
+test_that("degree of polyMatrix", {
+  expect_equal(degree(polyMatrix(0, 4, 5, 1000)), 0)
+  expect_equal(degree(polyMatrix(1, 2, 5, 5)), 5)
+})
+
