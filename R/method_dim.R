83f18dd87f13a81831d67d93c2239fc0e5a9e228
Nikolai Ryzhkov <namezys@gmail.com>
2020-10-20 22:08:41 +0200

    dim_for_polynomial

--- R/method_dim.R
+++ R/method_dim.R
@@ -6,11 +6,14 @@ setGeneric("nrow", nrow)
 setMethod("nrow", signature(x = PM), function(x) {
   return(nrow(x@coef))
 })
+setMethod("nrow", signature(x = P), function(...) { 1 })
 
 setGeneric("ncol", ncol)
 setMethod("ncol", signature(x = PM), function(x) {
   return(x@ncol)
 })
+setMethod("ncol", signature(x = P), function(x) { 1 })
 
 setGeneric("dim")
 setMethod("dim", signature(x = PM), function(x) { c(nrow(x), ncol(x)) })
+setMethod("dim", signature(x = P), function(x) { c(1, 1) })
--- tests/testthat/test_method_dim.R
+++ tests/testthat/test_method_dim.R
@@ -5,12 +5,16 @@ test_that("nrow", {
   expect_equal(nrow(polyMatrix(1, 3, 7)), 3)
   expect_equal(nrow(polyMatrix(1, 4, 2, 11)), 4)
   expect_equal(nrow(polyMatrix(0, 2, 9, 11)), 2)
+
+  expect_equal(nrow(p(1, 2, 3, 4)), 1)
 })
 test_that("ncol", {
   expect_equal(ncol(matrix(1, 6, 4)), 4)
   expect_equal(ncol(polyMatrix(1, 3, 7)), 7)
   expect_equal(ncol(polyMatrix(1, 4, 2, 11)), 2)
   expect_equal(ncol(polyMatrix(0, 2, 9, 11)), 9)
+
+  expect_equal(ncol(p(1, 4, 5, 6)), 1)
 })
 
 test_that("dim", {
@@ -18,4 +22,6 @@ test_that("dim", {
   expect_equal(dim(polyMatrix(1, 3, 7)), c(3, 7))
   expect_equal(dim(polyMatrix(1, 4, 2, 11)), c(4, 2))
   expect_equal(dim(polyMatrix(0, 2, 9, 11)), c(2, 9))
+
+  expect_equal(dim(p(3, 4, 5, 6, 9)), c(1, 1))
 })
